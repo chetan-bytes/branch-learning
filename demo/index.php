fc1224563bdcc831af8f7107e6f6bcd823d38a10
urvish <urvish.patel@bytestechnolab.com>
2019-08-16 17:37:05 +0530

    changes in index.php

--- demo/index.php
+++ demo/index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 	<h1>Test Demo</h1>
+	<p>The deciding hand braves a minister. The reign swings an icon. A doom forgives a talent. The guitar reasons within the archive!</p>
 </body>
 </html>
