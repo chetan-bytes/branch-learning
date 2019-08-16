15e6ddbc3254f7769a928ea7cbd36b4a14280f08
Hetal <hetal.kanani@bytestechnolab.com>
2019-08-16 13:10:35 +0530

    save changes

--- file.php
+++ file.php
@@ -2,9 +2,5 @@ text is written by urvish
 file change uploaded.
 Text is written by urvish.
 Text is written by Hetal.
-<<<<<<< HEAD
 Text is written by Hetal12.
-=======
-
 this is chetan text.
->>>>>>> 850d9ab0bb44780a3d18ff2b552f50cefac20009
