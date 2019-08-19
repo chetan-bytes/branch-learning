41afe32f97616662b7a8747484e4ec725a182a58
urvish <urvish.patel@bytestechnolab.com>
2019-08-19 12:21:58 +0530

    Revert "reset delete folder"
    This reverts commit 44ce95a66381543cd6a4c504840b48f49ec0b02e.

---
+++ demo/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h1>Test Demo</h1>
+	<p>The deciding hand braves a minister. The reign swings an icon. A doom forgives a talent. The guitar reasons within the archive!</p>
+</body>
+</html>
